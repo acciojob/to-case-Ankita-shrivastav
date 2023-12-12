d30b7b64c13c62581a8593b1c02e6146ae155126
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-13 00:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function toCase(text) {
   // write your code here
+	if(text===''){
+		return'-';
+	}
+	const lowercase=text.toLowerCase();
+	const uppercase=text.toUpperCase();
+
+	return `${lowercase}-${uppercase}`;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
